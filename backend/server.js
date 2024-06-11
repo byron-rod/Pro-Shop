410e39d25ef9d09c202b2694e1a62e42c0ae970f
Byron <byron303@gmail.com>
2024-06-11 16:18:55 -0600

    creating paypal payment

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/ProductRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import recurrenteRoutes from "./routes/recurrenteRoutes.js";
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -24,6 +26,14 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/recurrente", recurrenteRoutes);
+
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
+app.get("");
 
 app.use(notFound);
 app.use(errorHandler);
--- frontend/src/components/CheckoutSteps.jsx
+++ frontend/src/components/CheckoutSteps.jsx
@@ -1,44 +1,41 @@
 import { Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { useLocation } from "react-router-dom";
+import "./CSS/CheckoutSteps.css"; // Import the CSS file
 
-const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
+const CheckoutSteps = () => {
+  const location = useLocation();
+  const path = location.pathname;
+
   return (
     <Nav className="justify-content-center mb-4">
       <Nav.Item>
-        {step1 ? (
-          <LinkContainer to="/login">
-            <Nav.Link>Sign In</Nav.Link>
-          </LinkContainer>
-        ) : (
-          <Nav.Link disabled>Sign In</Nav.Link>
-        )}
+        <LinkContainer to="/login">
+          <Nav.Link className={path === "/login" ? "active-step" : ""}>
+            Sign In
+          </Nav.Link>
+        </LinkContainer>
       </Nav.Item>
       <Nav.Item>
-        {step2 ? (
-          <LinkContainer to="/shipping">
-            <Nav.Link>Shipping</Nav.Link>
-          </LinkContainer>
-        ) : (
-          <Nav.Link disabled>Shipping</Nav.Link>
-        )}
+        <LinkContainer to="/shipping">
+          <Nav.Link className={path === "/shipping" ? "active-step" : ""}>
+            Shipping
+          </Nav.Link>
+        </LinkContainer>
       </Nav.Item>
       <Nav.Item>
-        {step3 ? (
-          <LinkContainer to="/payment">
-            <Nav.Link>Payment</Nav.Link>
-          </LinkContainer>
-        ) : (
-          <Nav.Link disabled>Payment</Nav.Link>
-        )}
+        <LinkContainer to="/payment">
+          <Nav.Link className={path === "/payment" ? "active-step" : ""}>
+            Payment
+          </Nav.Link>
+        </LinkContainer>
       </Nav.Item>
       <Nav.Item>
-        {step4 ? (
-          <LinkContainer to="/placeorder">
-            <Nav.Link>Place Order</Nav.Link>
-          </LinkContainer>
-        ) : (
-          <Nav.Link disabled>Place Order</Nav.Link>
-        )}
+        <LinkContainer to="/placeorder">
+          <Nav.Link className={path === "/placeorder" ? "active-step" : ""}>
+            Place Order
+          </Nav.Link>
+        </LinkContainer>
       </Nav.Item>
     </Nav>
   );
